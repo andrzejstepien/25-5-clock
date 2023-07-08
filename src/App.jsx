05bff2c7274dd86815ad6a27e1d35dfd87d7f9cf
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-07-08 20:58:37 +0200

    delay

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 import './App.css'
 
@@ -7,7 +7,20 @@ function App() {
     const [breakLength, setBreakLength] = useState(5)
     const [sessionLength, setSessionLength] = useState(25)
     const [timeLeft, setTimeLeft] = useState(sessionLength)
-  
+    const [deadline, setDeadline] = useState(new Date().getTime())
+
+    const calculateTimeLeft = ()=>{
+      let current = new Date().getTime()
+      return deadline-current
+    }
+
+    const delay = ms => new Promise(res=> setTimeout(res,ms))
+
+    delay(5000).then(console.log(calculateTimeLeft()))
+    
+    
+
+
     return (
       <div className="App">
         <header><h1>25 + 5 Clock</h1></header>
@@ -15,6 +28,9 @@ function App() {
       <p id="break-length">{breakLength}</p>
       <p id="session-label">Session</p>
       <p id="session-length">{sessionLength}</p>
+      <p id="timer-label">Timer</p>
+      <p id="time-left">{timeLeft}</p>
+
       </div>
     );
   

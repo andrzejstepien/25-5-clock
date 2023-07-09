5ab550eba49674724589da7debe75c550ab44262
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-07-09 19:36:40 +0200

    ALL TESTS PASS!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
-
+import gong from "/src/assets/411574__inspectorj__alto-gong-metal-hit-b-h6-xy.wav"
 import './App.css'
 function App() {
 
@@ -7,6 +7,7 @@ function App() {
     const [sessionLength, setSessionLength] = useState(25)
     const [timeLeftSeconds, setTimeLeftSeconds] = useState(sessionLength*60)
     const [isRunning, setisRunning] = useState(false)
+    const [phase, setPhase] = useState("session")
 
     useEffect(()=>{
       let timer = null
@@ -15,17 +16,36 @@ function App() {
         timer = setInterval(()=>{
           setTimeLeftSeconds(prev=>{
             if(prev>0){return prev-interval/1000
-          }else{return 0}  
+          }else{
+            phaseChange()
+            return 0}  
           })
         },interval)
       }else{clearInterval(timer)}
       return ()=>{clearInterval(timer)}
-    },[isRunning])
+    },[isRunning,phase])
 
     useEffect(()=>{
-      setTimeLeftSeconds(sessionLength*60)
-    },[sessionLength])
+      setTimeLeftSeconds(phaseLengthSeconds[phase]())
+    },[sessionLength,phase])
 
+
+
+    const phaseChange = ()=>{
+      if(phase==="session"){
+        setPhase("break")
+      }else if(phase==="break"){
+        setPhase("session")
+      }
+      document.getElementById("beep").play()
+    }
+
+    const phaseLengthSeconds = {
+      break:function(){
+        console.log("BREAK TIME!")
+        return breakLength*60},
+      session:function(){return sessionLength*60},
+    }
 
 
     const formatTimeLeft = (timeLeftSeconds)=>{
@@ -45,9 +65,12 @@ function App() {
       }else{setisRunning(true)}
     }
     const reset = ()=>{
-      setisRunning(true)
+      document.getElementById("beep").pause()
+      document.getElementById("beep").currentTime = 0
+      setisRunning(false)
       setBreakLength(5)
       setSessionLength(25)
+      setPhase("session")
       setTimeLeftSeconds(sessionLength*60)
       setisRunning(false)
     }
@@ -70,18 +93,19 @@ function App() {
         <header><h1>25 + 5 Clock</h1></header>
       <p id="break-label">Break</p>
       <p id="break-length">{breakLength}</p>
-      <button id="break-decrement" onClick={()=>incrementBreak(1)}>MORE</button>
-      <button id="break-increment" onClick={()=>incrementBreak(-1)}>LESS</button>
+      <button id="break-increment" onClick={()=>incrementBreak(1)}>MORE</button>
+      <button id="break-decrement" onClick={()=>incrementBreak(-1)}>LESS</button>
       <p id="session-label">Session</p>
       <p id="session-length">{sessionLength}</p>
       <button id="session-increment" onClick={()=>incrementSession(1)}>MORE</button>
       <button id="session-decrement" onClick={()=>incrementSession(-1)}>LESS</button>
-      <p id="timer-label">Timer</p>
+      <p id="timer-label">{phase=="break"?"BREAK TIME!":"YOU CAN DO IT!"}</p>
       <p id="time-left">{formatTimeLeft(timeLeftSeconds)}</p>
-      <button id="start_stop" onClick={pause}>PAUSE</button>
-      <p>{isRunning?"isRunning":"NOT isRunning"}</p>
+      <button id="start_stop" onClick={pause}>{isRunning?"STOP":"START"}</button>
+      <p>{isRunning?"isRunning":"NOT Running"}</p>
+      <p>{phase}</p>
       <button id="reset" onClick={reset}>RESET</button>
-
+      <audio id="beep" src={gong}></audio>
 
       </div>
     );
